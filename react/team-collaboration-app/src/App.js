9ee17642bf0ea78dd63f433f5af70b066f6b0f52
Abrar <abrar9597@gmail.com>
2021-05-29 21:36:35 +0600

    app data fetch works

--- react/team-collaboration-app/src/App.js
+++ react/team-collaboration-app/src/App.js
@@ -12,63 +12,46 @@ export const currProjContext = React.createContext();
 
 function App() {
 
-  const [projects, setProjects] = useState([]);
-  const [currProj, setCurrProj] = useState({});
-  
- /* const dataFetch = useCallback(() => {
-      axios.get('http://localhost:8080/api/project')
-      .then(res => {
-        setProjects(res.data);
-        setCurrProj(projects[0]);
-        console.log(projects)
-        console.log(currProj)
-  
-      }).catch(err => {
-        console.log(err);
-      })
-    },[])
+  const [projects, setProjects] = useState(null);
+  const [currProj, setCurrProj] = useState(null);
+
+  const dataFetch = async () => {
+    let res = await axios.get('http://localhost:8080/api/project')
+
+    setProjects(res.data);
+    setCurrProj(res.data[0]);
+    console.log(projects)
+    console.log(currProj)
 
 
-  useEffect(() => {dataFetch()},[dataFetch]) */
+  }
 
- /* const newSetProjects = () =>
-  {
-     
-  }  */
+  useEffect(() => {
+    dataFetch();
 
-/*  useEffect(async ()=>
-  {
-      let res = await axios.get('http://localhost:8080/api/project')
-      
-       setProjects(res.data);
-       setCurrProj(projects[0]);
-      
+  }, [dataFetch])
 
-        console.log(res)
-        console.log(projects)
-        console.log(currProj)
 
-     },[]) */
-
-  
   return (
-   
-   // <h1>{currProj?.Name}</h1> 
-    
-      <div className='page-Style'>
-        <HeaderContainer/>
-        <div className='bottom-Style'>
-          <currProjContext.Provider value = {[currProj,setCurrProj]}>
-          <ProjectContainer projects = {projects}/>
-          </currProjContext.Provider>
-          <RoomsContainer project={currProj}/>
-          <div className = 'taskWork-Style'>
-            <NavBar/>
-            <WorkContainer/> 
-          </div>
+
+    <h1>{currProj?.Name}</h1>
+
+    /*
+    <div className='page-Style'>
+      <HeaderContainer />
+      <div className='bottom-Style'>
+        <currProjContext.Provider value={[currProj, setCurrProj]}>
+          <ProjectContainer projects={projects} />
+        </currProjContext.Provider>
+        <RoomsContainer project={currProj} />
+        <div className='taskWork-Style'>
+          <NavBar />
+          <WorkContainer />
         </div>
       </div>
-     
+    </div>
+    */
+
   );
 }
 
